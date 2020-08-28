ed0753adceec0e204584652923a1ecb494320bdc
sxsg430 <872597+sxsg430@users.noreply.github.com>
2020-08-28 13:10:55 +1000

    Basic error handling for Jikan errors

--- routes/data.js
+++ routes/data.js
@@ -5,7 +5,8 @@ const Jikan = require('jikan-node');
 router.get('/:show', function(req, res, next) {
   const mal = new Jikan();
   let maldat = mal.findAnime(req.params['show'])
-  .then(info => res.json(info));
+  .then(info => res.json(info))
+  .catch(err => res.send(err));
 });
 
 module.exports = router;
