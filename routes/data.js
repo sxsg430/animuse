c9eeebf7f2589377f90ff7cbd38f7d496623a60e
sxsg430 <872597+sxsg430@users.noreply.github.com>
2020-08-28 14:12:14 +1000

    Replace spotify API and configure with test song

--- routes/data.js
+++ routes/data.js
@@ -1,12 +1,26 @@
 var express = require('express');
 var router = express.Router();
 const Jikan = require('jikan-node');
+const Spotify = require('node-spotify-api');
+
 /* GET home page. */
 router.get('/:show', function(req, res, next) {
   const mal = new Jikan();
-  let maldat = mal.findAnime(req.params['show'])
-  .then(info => res.json(info))
-  .catch(err => res.send(err));
+  var spotify = new Spotify({
+    id: process.env.SPOTIFY_CLIENT,
+    secret: process.env.SPOTIFY_SECRET
+  });
+  let maldat = mal.findAnime(req.params['show']);
+
+  spotify.search({type: 'track', query: 'Synchrogazer'})
+  .then(function(response) {
+    res.json(response);
+  })
+  .catch(function(err) {
+    res.json(err)
+  });
+  //.then(info => res.json(info))
+  //.catch(err => res.send(err));
 });
 
 module.exports = router;
