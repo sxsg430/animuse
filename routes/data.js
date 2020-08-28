86288e1065078d72019d3a4319098a28f36c524c
sxsg430 <872597+sxsg430@users.noreply.github.com>
2020-08-28 13:09:02 +1000

    Use route to get show ID

--- routes/data.js
+++ routes/data.js
@@ -2,9 +2,9 @@ var express = require('express');
 var router = express.Router();
 const Jikan = require('jikan-node');
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/:show', function(req, res, next) {
   const mal = new Jikan();
-  let maldat = mal.findAnime('11597')
+  let maldat = mal.findAnime(req.params['show'])
   .then(info => res.json(info));
 });
 
