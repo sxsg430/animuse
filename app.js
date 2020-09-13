9e0a2fbe65312f6c0eda1ee613fa3c22bf7df45f
sxsg430 <872597+sxsg430@users.noreply.github.com>
2020-09-13 18:06:34 +1000

    Add initial page for searching

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var usersRouter = require('./routes/users');
 var dataRouter = require('./routes/data');
 var spotifyRouter = require('./routes/spotify');
 var discogsRouter = require('./routes/discogs');
+var searchRouter = require('./routes/search');
 
 var app = express();
 
@@ -27,6 +28,7 @@ app.use('/users', usersRouter);
 app.use('/showdata', dataRouter);
 app.use('/spotifyreq', spotifyRouter);
 app.use('/discogsreq', discogsRouter);
+app.use('/search', searchRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
