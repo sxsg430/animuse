ac8fc49e6d61d22e9d0f84f18c11a827df9ae332
sxsg430 <872597+sxsg430@users.noreply.github.com>
2020-09-14 11:37:47 +1000

    Update to handle breaking Jikan changes

--- routes/spotify.js
+++ routes/spotify.js
@@ -3,13 +3,14 @@ var router = express.Router();
 const Spotify = require('node-spotify-api');
 
 /* GET users listing. */
-router.get('/:song', function(req, res, next) {
+router.get('/', function(req, res, next) {
   var spotify = new Spotify({
     id: process.env.SPOTIFY_CLIENT,
     secret: process.env.SPOTIFY_SECRET
   });
   let cleanupSongString = function(songStr) {
-    let finalStr = songStr.split(' by ')[0].replace('\"', '').replace('\"', '').replace(')', '').split(' (');
+    let reg = /^\d:\s/gi;
+    let finalStr = songStr.split(' by ')[0].replace(reg, '').replace('\"', '').replace('\"', '').replace(')', '').split(' (');
     let artistStr = songStr.split(' by ')[1].replace(')', '').split(' (');
     //.replace('\"', '').replace('\"', '').replace(')', '').split('and').split(' (')[0];
     let artistArr = songStr.split(' by ')[1].split(' (');
@@ -24,7 +25,7 @@ router.get('/:song', function(req, res, next) {
     console.log(out);
     return out;
   }
-  spotify.search({type: 'track', query: cleanupSongString(req.params['song'])[0]})
+  spotify.search({type: 'track', query: cleanupSongString(req.query.song)[0]})
   .then((data) => {
     if (data['tracks'].items.length > 0) { // If array of tracks is length of 0, assume no matches and return error message.
       return res.json(data['tracks']['items'][0]['uri']);
