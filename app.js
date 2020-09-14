0053931262f1129a9656216c949da635128298dc
sxsg430 <872597+sxsg430@users.noreply.github.com>
2020-09-14 14:32:35 +1000

    Fix issues with CORS

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
 var config = require('dotenv').config();
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -12,6 +13,8 @@ var discogsRouter = require('./routes/discogs');
 var searchRouter = require('./routes/search');
 
 var app = express();
+
+app.use(cors())
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- routes/spotify.js
+++ routes/spotify.js
@@ -4,6 +4,8 @@ const Spotify = require('node-spotify-api');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   var spotify = new Spotify({
     id: process.env.SPOTIFY_CLIENT,
     secret: process.env.SPOTIFY_SECRET
