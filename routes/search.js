39344cdd8ce205c3a7e6ebd0026aa087b56c317c
sxsg430 <872597+sxsg430@users.noreply.github.com>
2020-09-14 21:04:24 +1000

    Tweak search

--- routes/search.js
+++ routes/search.js
@@ -8,11 +8,18 @@ router.get('/', async (req, res, next) => {
 
   let maldat = mal.search('anime', req.query.title, {})
   .then(info => {
-    let finalJson = {
+    if (info.status == "429") {
+      let finalJson2 = {
+        response: "Ratelimited",
+      }
+      res.json(finalJson2);
+    } else {
+      let finalJson = {
         response: "N/A",
         search: info.results
       }
-    res.json(finalJson);
+      res.json(finalJson);
+    }
   })  
 });
 
