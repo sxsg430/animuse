5195451208038ef11d8900b90b6aec9e27e16fb0
sxsg430 <872597+sxsg430@users.noreply.github.com>
2020-09-15 19:14:36 +1000

    Cleanup old endpoints

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var logger = require('morgan');
 var cors = require('cors')
 var config = require('dotenv').config();
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var dataRouter = require('./routes/data');
 var spotifyRouter = require('./routes/spotify');
 var discogsRouter = require('./routes/discogs');
@@ -19,7 +18,7 @@ app.use(cors())
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-app.use(express.static('../animuse-client/build'))
+//app.use(express.static('../animuse-client/build'))
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -27,7 +26,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/showdata', dataRouter);
 app.use('/spotifyreq', spotifyRouter);
 app.use('/discogsreq', discogsRouter);
